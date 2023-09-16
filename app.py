461325dde331bdecc37350b5c74192ff9100f8d4
ratanak784 <145197917+ratanak784@users.noreply.github.com>
2023-09-16 19:14:39 +0700

    Add files via upload

---
+++ app.py
@@ -0,0 +1,22 @@
+import streamlit as st
+
+st.set_page_config(page_title="About Ratanak", page_icon=":alien_monster:", layout="wide")
+
+with st.container():
+    st.subheader("Hi, I'm Ratanak :wave:")
+    st.title("A student from Cambodia")
+    st.write("I'm passionate about learning a new language, meet new people, and using my mind as my weapon.")
+    st.write("[Our GroupChat for the boys of 10 'A'](https://t.me/+vyFYQpMnP6QxYTJl)")
+with st.container():
+    st.write("---")
+    st.header("My Projects")
+    st.write("##")
+    st.write(
+        """
+        - I'm from Phnom Penh, Cambodia.
+        - I'm a sophomore or a tenth grader in high school.
+        - Lastly, I studied at PSIS-NOHP.
+        """
+    )
+    st.write("More information about my school here.")
+    st.write("[PSIS-NOHP Group](https://t.me/+ruG1VAMbKx45ZGY1)")
